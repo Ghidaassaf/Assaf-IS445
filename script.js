1e7723ab6d0731c1c21d949a87c3cde83dc1462b
Ghida Assaf <ghidaassaf@hotmail.com>
2013-03-17 01:01:22 -0700

    HW5 FINAL INDEX.HTML AND SCRIPT.HTML

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-function enterNumber_prompt() 
-{
-
-    document.writeln("<h1>Assaf, Ghida  Homework 5</h1>")
+﻿function enterNumber_prompt() {
+    document.writeln("<h1>Assaf Ghida - Homework 5</h1>")
     var number = prompt("Please enter a number between 2 and 10:", "")
 
 
-if (number < 2 || number > 10)
-{
-
-document.writeln("<h2>Your input is " + number + ". The valid input number is between 2 and 10. Please reload this page and try again.")
+    if (number < 2 || number > 10) {
+        document.writeln("<h2>Your input is " + number + ". The valid input number is between 2 and 10. Please reload this page and try again.")
     }
-
     else {
         document.writeln("<h2>Your input number is " + number + " </h2>")
         var time = 0
@@ -19,21 +14,19 @@ document.writeln("<h2>Your input is " + number + ". The valid input number is be
             result /= 2
             time += 1
         }
-       
- document.writeln("<h2>The number of times to divide the number " + number + " by 2 to get a value less than one millionth is " + time + "</h2>")
+        document.writeln("<h2>The number of times to divide the number " + number + " by 2 to get a value less than one millionth is " + time + "</h2>")
 
-        
-	var star = "<div>"
+        var star = "<div>"
         var lineNumber = number
-        for (var i = 0; i < lineNumber; i++) {
-            for (var j = lineNumber-i; j > 0; j--) {
+        for (var line = 0; line < lineNumber; line++) {
+            for (var j = lineNumber-line; j > 0; j--) {
                 star += "*"
             }
             
             star += "<br>"
         }
-
         document.writeln(star + "</div>")
-  
-  }
+
+
+    }
 }
