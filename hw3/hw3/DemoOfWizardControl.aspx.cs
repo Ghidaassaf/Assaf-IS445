bc8a4fcc37dcc6d6c4a175baf1107ab1650366b1
Ghida Assaf <ghidaassaf@hotmail.com>
2013-04-14 15:06:55 -0700

    homework 6 saved final

---
+++ hw3/hw3/DemoOfWizardControl.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace hw3
+{
+    public partial class DemoOfWizardControl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Wizard1_FinishButtonClick(object sender, WizardNavigationEventArgs e)
+        {
+            SummaryName.Text = UserName.Text;
+            SummaryCard.Text = CreditCard.Text;
+            SummaryDate.Text = Calendar1.SelectedDate.ToString();
+        }
+    }
+}
